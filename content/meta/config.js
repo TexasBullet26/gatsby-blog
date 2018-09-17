d70dc624c0d48eb9c7b4734606585823bd9413d5
Glenn Lanzer <lanzertrey@gmail.com>
2018-09-17 11:17:36 -0500

    Edit: some values in config.js

--- content/meta/config.js
+++ content/meta/config.js
@@ -1,32 +1,32 @@
 const colors = require("../../src/styles/colors");
 
 module.exports = {
-  siteTitle: "PersonalBlog - a blog starter for GatsbyJS", // <title>
-  shortSiteTitle: "PersonalBlog GatsbyJS Starter", // <title> ending for posts and pages
-  siteDescription: "PersonalBlog is a GatsbyJS starter.",
+  siteTitle: "Software developer, Writer, Learner, Athlete, and much more!", // <title>
+  shortSiteTitle: "Trey Lanzer Personal Blog", // <title> ending for posts and pages
+  siteDescription: "Blog by Trey Lanzer",
   siteUrl: "https://gatsby-starter-personal-blog.greglobinski.com",
   pathPrefix: "",
   siteImage: "preview.jpg",
   siteLanguage: "en",
   // author
-  authorName: "greg lobinski",
-  authorTwitterAccount: "greglobinski",
+  authorName: "Trey Lanzer",
+  authorTwitterAccount: "LanzerTrey26",
   // info
-  infoTitle: "greg lobinski",
+  infoTitle: "Trey Lanzer",
   infoTitleNote: "personal blog",
   // manifest.json
-  manifestName: "PersonalBlog - a blog starter for GatsbyJS",
+  manifestName: "PersonalBlog - Trey Lanzer's Blog",
   manifestShortName: "PersonalBlog", // max 12 characters
   manifestStartUrl: "/",
   manifestBackgroundColor: colors.background,
   manifestThemeColor: colors.background,
   manifestDisplay: "standalone",
   // contact
-  contactEmail: "john@doe.com",
+  contactEmail: "lanzertrey@google.com",
   // social
   authorSocialLinks: [
-    { name: "github", url: "https://github.com/greglobinski" },
-    { name: "twitter", url: "https://twitter.com/greglobinski" },
-    { name: "facebook", url: "http://facebook.com/greglobinski" }
+    { name: "github", url: "https://github.com/TexasBullet26" },
+    { name: "twitter", url: "https://twitter.com/LanzerTrey26" },
+    { name: "facebook", url: "http://facebook.com/trey.lanzer" }
   ]
 };
